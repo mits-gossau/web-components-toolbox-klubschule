a2a5b95e7415ec9fdc6a284217987e93acf6cadd
Weedshaker <weedshaker@gmail.com>
2023-12-05 12:22:57 +0100

    [MIDUWEB-142] visual btn regression test

---
+++ e2e/button.spec.js
@@ -0,0 +1,26 @@
+const { test, expect } = require('@playwright/test')
+
+const dir = process.cwd().split('/').pop().split('-')
+const PROJECT_NAME = dir.splice(3)[0]
+const WAITING_TIMEOUT = 200
+
+test('button test', async ({ page, browserName }) => {
+  await page.goto('src/es/components/pages/Buttons.html')
+  const demoPage = await page.waitForSelector('body')
+  const wcLoaded = await demoPage.getAttribute('wc-config-load')
+  console.log('wait for wc-load');
+  if (wcLoaded) {
+    console.log('loaded');
+    await page.evaluate(() => document.fonts.ready)
+    await page.waitForTimeout(WAITING_TIMEOUT)
+    await page.evaluate(() => window.scrollTo(0, 999999999))
+    await page.waitForTimeout(WAITING_TIMEOUT)
+    await page.evaluate(() => window.scrollTo(0, 999999999))
+    await page.waitForTimeout(WAITING_TIMEOUT)
+    await page.evaluate(() => window.scrollTo(0, 999999999))
+    await page.waitForTimeout(WAITING_TIMEOUT)
+    await page.evaluate(() => window.scrollTo(0, 0))
+    await page.waitForTimeout(WAITING_TIMEOUT)
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`${PROJECT_NAME}.png`)
+  }
+})
