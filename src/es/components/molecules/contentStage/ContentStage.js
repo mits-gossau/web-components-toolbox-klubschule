3de1105e447e31503b4d066358d2c5c9270e9bd0
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2024-01-09 10:43:42 +0100

    fix(ContentStage.js): fix variable assignment in constructor to use this keyword fix(ContentStage.js): fix variable assignment in renderHTML() to use this keyword
    chore(web-components-toolbox): update subproject commit hash

--- src/es/components/molecules/contentStage/ContentStage.js
+++ src/es/components/molecules/contentStage/ContentStage.js
@@ -4,6 +4,10 @@ import { Shadow } from '../../web-components-toolbox/src/es/components/prototype
 export default class ContentStage extends Shadow() {
   constructor (options = {}, ...args) {
     super({ importMetaUrl: import.meta.url, ...options }, ...args)
+
+    this.title = this.querySelector('h1').innerText;
+    this.subtitle = this.querySelector('p').innerText;
+    this.imageSrc = this.getAttribute('image-src');
   }
 
   connectedCallback () {
@@ -41,33 +45,21 @@ export default class ContentStage extends Shadow() {
 
   renderHTML () {
     this.html = ''
-    // const fetchModules = this.fetchModules([
-    //   {
-    //     path: `${this.importMetaUrl}'../../../../web-components-toolbox/src/es/components/organisms/grid/Grid.js`,
-    //     name: 'a-grid'
-    //   }
-    // ])
 
-    const title = this.root.querySelector('h1').innerText;
-    const subtitle = this.root.querySelector('p').innerText;
-    const imageSrc = this.getAttribute('image-src');
+    
 
     this.html = /* html */ `
         <o-grid namespace="grid-2colums2rows-" first-container-vertical first-column-with="66%" style="background-color:#0053A6; margin: 0; width: 100%;">
             <section>
                 <div style="display: contents; color: white; height: auto; margin: 3rem 1rem 2rem calc(4rem/3);">
-                    <h1 style="color: white; line-height: 1.5; margin-top: 1rem; margin-bottom: 32px; font-size: calc(8em/3);">${title}</h1>
-                    <p style="font-size: calc(10em/9);">${subtitle}</p>
+                    <h1 style="color: white; line-height: 1.5; margin-top: 1rem; margin-bottom: 32px; font-size: calc(8em/3);">${this.title}</h1>
+                    <p style="font-size: calc(10em/9);">${this.subtitle}</p>
                 </div>
                 <aside>
-                    <div slot="image" style="background-image:url('${imageSrc}'); background-position: center center; background-size: cover; height:100%; min-height: 280px;"></div>
+                    <div slot="image" style="background-image:url('${this.imageSrc}'); background-position: center center; background-size: cover; height:100%; min-height: 280px;"></div>
                 </aside>
             </section>
         </o-grid>
     `
-
-    Array.from(this.root.children).forEach(node => {
-      this.list.appendChild(node)
-    })
   }
 }
