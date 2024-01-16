edf9a5a091a3cf9cda7f225ecda5b6ef08f5323d
Thomas Deser <thomas.deser@merkle.com>
2024-01-16 17:34:28 +0100

    feat: add story teaser variant

--- src/es/components/molecules/teaser/Teaser.js
+++ src/es/components/molecules/teaser/Teaser.js
@@ -28,7 +28,9 @@ export default class KsTeaser extends Teaser {
   fetchTemplate () {
     switch (this.getAttribute('namespace')) {
       case 'teaser-tile-':
-        return this.fetchNamespaceTemplate('tile-/tile-.css')
+        return this.fetchNamespaceTemplate(['tile-/tile-.css'])
+      case 'teaser-story-':
+        return this.fetchNamespaceTemplate(['story-/story-.css'])
       default:
         return super.fetchTemplate()
     }
@@ -39,20 +41,21 @@ export default class KsTeaser extends Teaser {
    *
    * @return {Promise<void>}
    */
-  fetchNamespaceTemplate (relativePath) {
-    const styles = [
+  fetchNamespaceTemplate (relativePaths) {
+    const baseStyles = [
       {
         path: `${this.importMetaUrl}../../web-components-toolbox/src/css/style.css`,
         namespace: false
       }
     ]
 
-    return this.fetchCSS([{
+    const allStyles = relativePaths.map((relativePath) => ({
       // @ts-ignore
       path: `${this.importMetaUrl}${relativePath}`,
       namespace: false
-    }, ...styles
-    ]).then(fetchCSSParams => {
+    })).concat(baseStyles)
+
+    return this.fetchCSS(allStyles).then(fetchCSSParams => {
       // make template ${code} accessible aka. set the variables in the literal string
       fetchCSSParams[0].styleNode.textContent = eval('`' + fetchCSSParams[0].style + '`')// eslint-disable-line no-eval
     })
@@ -64,10 +67,78 @@ export default class KsTeaser extends Teaser {
    * @return {Promise<void>}
    */
   renderCSS () {
-    /* Emptied this css for now as it did not help for this teaser */
-    this.css = /* css */`
-        /* tbd */
-    `
+    this.setCss(/* css */`
+        :host * {
+          box-sizing: border-box;
+          margin: 0 !important;
+        }
+
+        :host a-picture {
+          display: block;
+          overflow: hidden;
+        }
+
+        :host figure {
+          display: flex;
+          flex-direction: column;
+          color: var(--color);
+        }
+
+        :host figcaption {
+          display: flex;
+          flex-direction: column;
+          gap: 0.75rem;
+          flex-shrink: 0;
+          width: 100%;
+        }
+
+        :host figcaption > * {
+          width: 100%;
+        }
+
+        :host figcaption > strong:first-child {
+          display: block;
+          font-family: var(--font-family-bold);
+          color: var(--color-${this.getAttribute('color')}, black);
+          font-family: var(--mdx-sys-font-fix-title4-font-family);
+          font-size: var(--mdx-sys-font-fix-title4-font-size);
+          font-weight: var(--mdx-sys-font-fix-title4-font-weight);
+          line-height: var(--mdx-sys-font-fix-title4-line-height);
+          transition: var(--transition);
+        }
+
+        :host h5,
+        :host h4,
+        :host h3,
+        :host h2,
+        :host h1 {
+          color: inherit !important;
+          transition: var(--transition);
+          font-family: var(--mdx-sys-font-flex-large-headline3-font-family) !important;
+          font-size: var(--mdx-sys-font-flex-large-headline3-font-size) !important;
+          font-weight: var(--mdx-sys-font-flex-large-headline3-font-weight) !important;
+          line-height: var(--mdx-sys-font-flex-large-headline3-line-height) !important;
+        }
+
+        :host p {
+          font-size: 1.125rem;
+          font-weight: 400;
+          line-height: 130%;
+          min-height: 5rem;
+          transition: var(--transition);
+        }
+
+        :host span {
+          display: flex;
+          gap: 0.25rem;
+          font-family: var(--font-family-secondary);
+          font-size: 1rem;
+          line-height: 110%;
+          font-weight: 500;
+          transition: var(--transition);
+        }
+    `, this.cssSelector, '') // unsetting namespace
+
     return this.fetchTemplate()
   }
 }
