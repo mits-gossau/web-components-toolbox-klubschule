bce5bfcfadb7c07f21bbdfa8db73801cda8dadda
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2024-01-24 10:07:42 +0100

    refactor(CenterFilterList.js): move centers and langOrder data to separate JSON file feat(CenterFilterList.js): import centers data from centers.json file
    fix(CenterFilterList.js): fix path for reset.css and style.css imports
    feat(CenterFilterList.js): add support for custom namespace and default- styles
    fix(CenterFilterList.js): fix language case to lowercase
    feat(CenterFilterList.js): add reset link with customizable text
    feat(CenterFilterList.js): render center filters based on language and region
    
    feat(centerFilterList): add centers.json file for language-specific center lists
    
    This commit adds the `centers.json` file to the `centerFilterList` component. The file contains language-specific center lists for the Swiss regions. The lists include centers for the German-speaking region, French-speaking region, and Italian-speaking region of Switzerland. Each region has its own set of centers listed in the JSON file.
    
    style(centerFilterList/default-.css): add styles for host element and headline label
    fix(pages/Center-Filter.html): update ks-m-center-filter-list component attributes

--- src/es/components/molecules/centerFilterList/CenterFilterList.js
+++ src/es/components/molecules/centerFilterList/CenterFilterList.js
@@ -1,26 +1,7 @@
 // @ts-check
 import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
 
-const centers = {
-    "Deutsche Schweiz" : [
-        "Aarau", "Aargau-Bern-und-Solothum", "Baden", "Basel", "Bern",
-        "Bern-Firmengeschaeft", "Bern-Welle7", "Brig", "Buchs-SG", "Chur",
-        "Davos-Klosters", "Frauenfeld", "Glarus", "Luzern", "Lyss",
-        "Olten", "Rapperswil", "Schaffhausen", "Sportpark-Dietikon", 
-        "St. Gallen", "St. Gallen Musikzentrum", "Sursee", "Thun", 
-        "Wetzikon", "Winterthur", "Zug", "Zürich Limmatplatz", 
-        "Zürich Oerlikon", "Zürich Altstetten", "Zürich Tanzwerk 101", 
-        "Zürich Angebote für Firmen"
-    ],
-    "Französische Schweiz": ["Fribourg", "Geneve", "La-Chaux-de-Fonds", "Lausanne", "Martigny", "Nyon", "Val-de-Ruz"],
-    "Italienische Schweiz": ["Bellinzona", "Lugano"]
-};
-
-const langOrder = {
-    "DE": ["Deutsche Schweiz", "Französische Schweiz", "Italienische Schweiz"],
-    "FR": ["Französische Schweiz", "Deutsche Schweiz", "Italienische Schweiz"],
-    "IT": ["Italienische Schweiz", "Französische Schweiz", "Deutsche Schweiz"]
-};
+import centers from './centers.json' assert { type: "json" };
 
 export default class CenterFilterList extends Shadow() {
     constructor (options = {}, ...args) {
@@ -45,55 +26,68 @@ export default class CenterFilterList extends Shadow() {
     fetchTemplate () {
       const styles = [
         {
-          path: `${this.importMetaUrl}../../../../es/components/web-components-toolbox/src/css/reset.css`, // no variables for this reason no namespace
+          path: `${this.importMetaUrl}../../../../es/components/web-components-toolbox/src/css/reset.css`,
           namespace: false
         },
         {
-          path: `${this.importMetaUrl}../../../../es/components/web-components-toolbox/src/css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+          path: `${this.importMetaUrl}../../../../es/components/web-components-toolbox/src/css/style.css`,
           namespaceFallback: true
         }
       ]
       switch (this.getAttribute('namespace')) {
+        case 'center-default-':
+          return this.fetchCSS([{
+            path: `${this.importMetaUrl}./default-/default-.css`,
+            namespace: false
+          }, ...styles])
         default:
           return this.fetchCSS(styles)
       }
     }
 
     renderHTML () {
-      const lang = this.getAttribute('lang') || "DE";
+      const lang = this.getAttribute('lang') || "de";
+
+      const resetParagraph = document.createElement('p')
+      const resetLink = document.createElement('a')
+      const resetText = this.getAttribute('reset-text') || "zurücksetzen";
+      resetLink.appendChild(document.createTextNode(resetText))
+      resetParagraph.appendChild(resetLink)
+      resetParagraph.classList.add('reset-link')
+
       const centerFilter = document.createElement('div')
+      centerFilter.appendChild(resetParagraph)
 
       this.fetchModules([{
           path: `${this.importMetaUrl}../../../../css/web-components-toolbox-migros-design-experience/src/es/components/organisms/MdxComponent.js`,
           name: 'mdx-component'
       }])
-  
-      langOrder[lang].forEach(region => {
-        if(centers[region]) {
-          // headline
-          const headline = document.createElement('h3');
-          headline.textContent = region;
-          centerFilter.appendChild(headline);
-  
-          centers[region].forEach(city => {
-            // checkbox
-            const checkbox = document.createElement('mdx-checkbox');
-            checkbox.setAttribute('variant', "no-border");
-            checkbox.setAttribute('label', city);
-  
-            const component = document.createElement('mdx-component');
-            
-            component.setAttribute('click-event-name', "mdx-component-click-event");
-            component.setAttribute('mutation-callback-event-name', "mdx-component-mutation-event");
-            component.setAttribute('listener-event-name', "mdx-set-attribute");
-            component.setAttribute('listener-detail-property-name', "attributes");
-  
-            component.appendChild(checkbox);
-  
-            centerFilter.appendChild(component);
-          });
-        }
-      });
+
+      Object.keys(centers[lang]).forEach(region => {
+        // label
+        const label = document.createElement('label')
+        label.classList.add('headline')
+        label.textContent = region
+        centerFilter.appendChild(label)
+
+        centers[lang][region].forEach(city => {
+          // checkbox
+          const checkbox = document.createElement('mdx-checkbox')
+          checkbox.setAttribute('variant', "no-border")
+          checkbox.setAttribute('label', city)
+
+          const component = document.createElement('mdx-component')
+
+          component.setAttribute('click-event-name', "mdx-component-click-event")
+          component.setAttribute('mutation-callback-event-name', "mdx-component-mutation-event")
+          component.setAttribute('listener-event-name', "mdx-set-attribute")
+          component.setAttribute('listener-detail-property-name', "attributes")
+
+          component.appendChild(checkbox)
+
+          centerFilter.appendChild(component)
+        })
+      })
 
       this.html = centerFilter
     }
