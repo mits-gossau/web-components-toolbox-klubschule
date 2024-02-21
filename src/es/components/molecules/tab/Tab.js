2782c3131c5133af494c2c1b17881a62299cc522
sturudic <srdan.turudic@emea.merkleinc.com>
2024-02-21 15:50:03 +0100

    feat(tab): MIDUWEB-378 add tab component

---
+++ src/es/components/molecules/tab/Tab.js
@@ -0,0 +1,116 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+
+export default class Tab extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+  }
+
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS()
+
+    const selectors = {
+      button: 'button',
+      buttonActive: 'button.active',
+      contentActive: 'div[tab-content-target]#'
+    }
+
+    const attributes = {
+      tabTarget: 'tab-target'
+    };
+
+    const states = {
+      active: 'active',
+      show: 'show'
+    };
+
+    // Show content of default active tab
+    this.tabs = this.root.querySelectorAll(selectors.button);
+    this.tabActive = this.root.querySelector(selectors.buttonActive);
+    this.tabActiveId = this.tabActive.getAttribute(attributes.tabTarget);
+    this.contentActive = this.root.querySelector(`${selectors.contentActive}${this.tabActiveId}`);
+    this.contentActive.classList.add(states.show);
+
+    // Handle changing active tabs
+    this.tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        this.tabActive.classList.remove(states.active);
+        this.contentActive.classList.remove(states.show);
+
+        tab.classList.add(states.active);
+        this.tabActive = tab;
+        this.tabActiveId = tab.getAttribute(attributes.tabTarget);
+        this.contentActive = this.root.querySelector(`${selectors.contentActive}${this.tabActiveId}`);
+        this.contentActive.classList.add(states.show);
+      });
+    });
+  }
+
+  disconnectedCallback () {
+    this.tabs.forEach(tab => {
+      tab.removeEventListener('click');
+    });
+  }
+
+  shouldRenderCSS () {
+    return !this.root.querySelector(
+      `:host > style[_css], ${this.tagName} > style[_css]`
+    )
+  }
+
+  renderCSS () {
+    this.css = /* css */ `
+        :host ul {
+          list-style: none;
+          display: flex;
+          margin: 0;
+          padding: 0;
+        }
+
+        :host ul li + li {
+          margin-left: 1.5em;
+        }
+
+        :host ul li button {
+          background-color: transparent;
+          border: none;
+          font-size: 1.125em;
+          line-height: 1.25em;
+          font-weight: 500;
+          cursor: pointer;
+          padding: 1em 0;
+        }
+
+        :host ul li button.active {
+          color: var(--mdx-base-color-klubschule-blue-600);
+          border-bottom: 2px solid var(--mdx-base-color-klubschule-blue-600);
+        }
+
+        :host div[tab-content-target] {
+          display: none;
+          padding: 3.375em 0 5em;
+        }
+
+        :host div[tab-content-target].show {
+          display: block;
+        }
+
+    `
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   */
+  fetchTemplate () {
+    /** @type {import("../../web-components-toolbox/src/es/components/prototypes/Shadow.js").fetchCSSParams[]} */
+    switch (this.getAttribute('namespace')) {
+        case 'tab-default-':
+            return this.fetchCSS([
+                {
+                path: `${this.importMetaUrl}./default-/default-.css`, // apply namespace since it is specific and no fallback
+                namespace: false
+            }])
+    }
+  }
+}
---
+++ src/es/components/molecules/tab/readme.md
@@ -0,0 +1,10 @@
+# tab
+
+> Generic tab component
+
+- [JIRA](https://jira.migros.net/browse/MIDUWEB-378)
+- [Example](../../pages/Tab.html)
+
+## Usage
+
+You can find a simple example markup in the above linked example page. To link a `button` (tab) and a content `div`, you have to set a name on the attribute `tab-target` on the `button` (tab) and use the same name in the attribute `id` of your tab content `div`. Each content `div` must have also an attribute called `tab-content-target`.
