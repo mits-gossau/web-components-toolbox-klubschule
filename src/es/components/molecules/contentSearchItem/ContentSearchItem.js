84b17418fee6942038729eea7a52149f2135493a
sturudic <srdan.turudic@emea.merkleinc.com>
2024-02-28 10:16:27 +0100

    feat(contentSearchItem): MIDUWEB-378 style component

--- src/es/components/molecules/contentSearchItem/ContentSearchItem.js
+++ src/es/components/molecules/contentSearchItem/ContentSearchItem.js
@@ -20,12 +20,56 @@ export default class ContentSearchItem extends Shadow() {
 
   renderCSS () {
     this.css = /* css */ `
-        :host div {
-          color: red;
+        :host {
+          display: block;
+          padding-bottom: 1.5rem;
+        }
+
+        :host a {
+          display: flex;
+          text-decoration: none;
+          color: var(--mdx-base-color-grey-950);
+        }
+
+        :host a:hover h3 {
+          color: var(--mdx-base-color-klubschule-blue-600);
+        }
+
+        :host div + a-picture {
+          margin-left: 1rem;
+        }
+
+        :host h3 {
+          font-size: 1rem;
+          line-height: 1.125rem;
+          font-weight: 500;
+          margin: 0;
+        }
+
+        :host p {
+          font-size: 1rem;
+          line-height: 1.25rem;
+          font-weight: 400;
+          margin: 0;
+          margin-top: 0.25rem;
+        }
+
+        :host p strong {
+          background-color: var(--mdx-base-color-klubschule-blue-600);
+          color: var(--mdx-base-color-grey-0);
+          font-weight: 400;
+          padding: 0 0.25rem;
         }
 
         @media only screen and (max-width: _max-width_) {
-          :host {
+          :host h3 {
+            line-height: 1.25rem;
+          }
+
+          :host p {
+            font-size: 0.875rem;
+            line-height: 1.125rem;
+            margin-top: 0.5rem;
           }
         }
     `
--- src/es/components/molecules/contentSearchItem/readme.md
+++ src/es/components/molecules/contentSearchItem/readme.md
@@ -4,3 +4,11 @@
 
 - [JIRA](https://jira.migros.net/browse/MIDUWEB-378)
 - [Example](../../pages/ContentSearchItem.html)
+
+## Usage
+
+To highlight words used in the search, wrap the work or text within the `p` tag with a `strong` tag:
+
+``` html
+<p>Some text <strong>highlighted</strong> more text.</p>
+```
