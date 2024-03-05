1bc1038594daa608c457cbae358e51b66f4ed226
Thomas Deser <thomas.deser@merkle.com>
2024-03-05 11:41:00 +0100

    feat: add logo list component

---
+++ src/es/components/molecules/logoList/LogoList.js
@@ -0,0 +1,61 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+
+export default class LinkList extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+  }
+
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS()
+  }
+
+  disconnectedCallback () {}
+
+  shouldRenderCSS () {
+    return !this.root.querySelector(
+      `:host > style[_css], ${this.tagName} > style[_css]`
+    )
+  }
+
+  renderCSS () {
+    this.css = /* css */ `
+        :host ul {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1.5rem;
+            list-style: none;
+            padding: 0;
+            align-items: center;
+        }
+
+        :host([centered]) ul {
+            text-align: center;
+            justify-content: center;
+        }
+
+        :host li a {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-top: 0.0625em solid var(--border-color);
+        }
+    `
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   */
+  fetchTemplate () {
+    /** @type {import("../../web-components-toolbox/src/es/components/prototypes/Shadow.js").fetchCSSParams[]} */
+    switch (this.getAttribute('namespace')) {
+      case 'logo-list-default-':
+        return this.fetchCSS([
+          {
+            path: `${this.importMetaUrl}./default-/default-.css`, // apply namespace since it is specific and no fallback
+            namespace: false
+          }])
+    }
+  }
+}
