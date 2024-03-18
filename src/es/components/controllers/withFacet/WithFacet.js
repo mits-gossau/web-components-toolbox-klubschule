f783d7b8fda9949e0dcf41cd05984f395876fef6
Alexander Schmidt <alexander@schmidt.eco>
2024-03-18 14:30:50 +0100

    refactor(WithFacet.js): remove unused URL and URLSearchParams initialization and console.log statements
    refactor(WithFacet.js): change constructFilterItem function to a method within the class
    feat(WithFacet.js): add logic to update filters based on URL parameters containing underscores
    
    refactor(WithFacet.js): rename 'url' variable to 'apiUrl' for clarity and consistency
    refactor(WithFacet.js): update fetch call to use 'apiUrl' variable instead of hardcoded url
    
    refactor(WithFacet.js): update parameter name from 'this.params' to 'params' throughout the file
    feat(WithFacet.js): add support for setting and deleting filter parameters based on selected child items
    feat(WithFacet.js): add support for updating URL with filter parameters after setting or deleting them
    
    refactor(WithFacet.js): extract constructFilterItem method for better readability and maintainability
    
    chore(FilterCategories.js): comment out assignment of this.html to this.mainNav in constructor
    
    refactor(FilterCategories.js): refactor code to improve readability and maintainability
    
    refactor(Angebotsliste-Suchergebnis.html): remove commented out code block in Angebotsliste-Suchergebnis.html

--- src/es/components/controllers/withFacet/WithFacet.js
+++ src/es/components/controllers/withFacet/WithFacet.js
@@ -36,9 +36,6 @@ export default class WithFacet extends Shadow() {
       ...options
     }, ...args)
 
-    this.url = new URL(window.location.href)
-    this.params = new URLSearchParams(this.url.search)
-    console.log('url + params', this.url, this.params.toString())
     const withFacetCache = new Map()
 
     this.isMocked = this.hasAttribute('mock')
@@ -47,51 +44,35 @@ export default class WithFacet extends Shadow() {
 
       console.log('---------------------------------event', event)
 
-      const constructFilterItem = (filterItem) => {
-        return filterItem
-          ? `{
-            "children": [
-              ${filterItem.children.map(child => {
-                const count = child.count ? `(${child.count})` : ''
-                const label = count ? `${child.label} ${count}` : child.label
-                const hasSameLabel = label.trim() === event.detail?.target.label.trim()
-                const isCheckedNullOrUndefined = event.detail?.target.checked === null || event.detail?.target.checked === undefined
-
-                return `{
-                  ${child.count ? `"count": ${child.count},` : ''}
-                  ${child.eTag ? `"eTag": "${child.eTag.replace(/"/g, '\\"')}",` : ''}
-                  "hasChilds": ${child.hasChilds},
-                  "id": "${child.id}",
-                  "label": "${child.label}",
-                  ${child.partitionKey ? `"partitionKey": "${child.partitionKey}",` : ''}
-                  ${child.rowKey ? `"rowKey": "${child.rowKey}",` : ''}
-                  "selected": ${hasSameLabel ? isCheckedNullOrUndefined ? child.selected : event.detail.target.checked : child.selected},
-                  ${child.sort ? `"sort": ${child.sort},` : ''}
-                  ${child.timestamp ? `"timestamp": "${child.timestamp}",` : ''}
-                  ${child.typ ? `"typ": "${child.typ}",` : ''}
-                  "urlpara": "${child.urlpara}"
-                }`
-              })}
-            ],
-            ${filterItem.disabled ? `"disabled": ${filterItem.disabled},` : ''}
-            ${filterItem.eTag ? `"eTag": "${filterItem.eTag.replace(/"/g, '\\"')}",` : ''}
-            ${filterItem.hasChilds ? `"hasChilds": ${filterItem.hasChilds},` : ''}
-            ${filterItem.id ? `"id": "${filterItem.id}",` : ''}
-            ${filterItem.label ? `"label": "${filterItem.label}",` : ''}
-            ${filterItem.options ? `"options": ${filterItem.options},` : ''}
-            ${filterItem.partitionKey ? `"partitionKey": "${filterItem.partitionKey}",` : ''}
-            ${filterItem.rowKey ? `"rowKey": "${filterItem.rowKey}",` : ''}
-            ${filterItem.sort ? `"sort": ${filterItem.sort},` : ''}
-            ${filterItem.timestamp ? `"timestamp": "${filterItem.timestamp}",` : ''}
-            ${filterItem.typ ? `"typ": "${filterItem.typ}",` : ''}
-            "visible": ${filterItem.visible || true}
-          }`
-          : ''
-      }
-
-      const filter = constructFilterItem(event.detail?.wrapper?.filterItem)
+      let selectedFilter = ''
       const filters = []
+      const filter = this.constructFilterItem(event)
       if (filter) filters.push(filter)
+      
+      // update filters according to url params
+      const url = new URL(self.location.href)
+      const params = new URLSearchParams(url.search)
+      const entriesWithUnderscore = [...params.entries()].filter(([key, value]) => key.includes('_') && value.includes('_'))
+      
+      entriesWithUnderscore.forEach(([key, value]) => {
+          console.log(`Key: ${key}, Value: ${value}`)
+
+          const [urlparaKey, idKey] = key.split('_')
+          const [urlparaValue, idValue] = value.split('_')
+
+          const filter = (`{
+              "urlpara": "${urlparaKey}",
+              "id": "${idKey}",
+              "selected": true,
+              "children": [{
+                  "urlpara": "${urlparaValue}",
+                  "id": "${idValue}",
+                  "selected": true
+              }]
+          }`)
+
+          filters.push(filter)
+      })
 
       const request = `{
         "filter": ${filters.length > 0 ? `[${filters.join(',')}]` : '[]'},
@@ -99,9 +80,7 @@ export default class WithFacet extends Shadow() {
         ${event.detail?.key === 'input-search' ? `,"searchText": "${event.detail.value}"` : ''}
       }`
 
-      // @ts-ignore
-      // console.log('request (WithFacet.js)', request, self.data = event.detail?.wrapper.filterItem)
-      const url = this.isMocked
+      const apiUrl = this.isMocked
         ? `${this.importMetaUrl}./mock/default.json`
         : `${this.getAttribute('endpoint') || 'https://miducabulaliwebappdev.azurewebsites.net/api/CourseSearch/withfacet'}`
 
@@ -128,7 +107,7 @@ export default class WithFacet extends Shadow() {
             ? withFacetCache.get(request)
             // TODO: withFacetCache key must include all variants as well as future payloads
             // TODO: know the api data change cycle and use timestamps if that would be shorter than the session life time
-            : withFacetCache.set(request, fetch(url, requestInit).then(response => {
+            : withFacetCache.set(request, fetch(apiUrl, requestInit).then(response => {
               if (response.status >= 200 && response.status <= 299) {
                 console.log('response (WithFacet.js)', response)
                 return response.json()
@@ -136,7 +115,6 @@ export default class WithFacet extends Shadow() {
               throw new Error(response.statusText)
             }).then(json => {
               const filterData = json.filters
-              let selectedFilter = ''
               let numberOfOffers = 0
 
               filterData.forEach(filterItem => {
@@ -144,59 +122,50 @@ export default class WithFacet extends Shadow() {
 
                 // set selected filter to url params
                 if (filterItem.children && filterItem.children.length > 0 && filterItem.visible) {
-                  const currentParams = this.params.get(filterItem.urlpara)?.split(',')
-                  console.log('currentParams', currentParams)
+                  const currentParams = params.get(filterItem.urlpara)?.split(',')
+                  console.log('currentParams', filterItem.urlpara, currentParams)
 
                   filterItem.children.forEach(child => {
                     if (child.selected) {
+                      console.log('selected:', child.urlpara)
                       // API does not answer with number of totals, the line below fixes that issue
                       if (child.count > 0) {
                         numberOfOffers += child.count
                       }
-                      console.log('selected:', child.urlpara)
-                      const currentValues = this.params.get(filterItem.urlpara) || ''
+                      
+                      const currentValues = params.get(filterItem.urlpara) || ''
 
                       if (!currentValues || !currentValues.includes(child.urlpara)) {
                         console.log('setting:', child.urlpara)
-                        this.params.set(filterItem.urlpara, `${currentValues + ',' || ''}${child.urlpara}`)
+                        params.set(`${filterItem.urlpara}_${filterItem.id}`, `${currentValues + ',' || ''}${child.urlpara}_${child.id}`)
                       }
                     } else {
-                      const currentValues = this.params.get(filterItem.urlpara)?.split(',')
+                      console.log('not selected:', child.urlpara)
+                      const currentValues = params.get(filterItem.urlpara)?.split(',')
                       let index
                       if (currentValues) {
-                        console.log(currentValues, index, currentValues.indexOf(child.urlpara))
+                        console.log(currentValues, currentValues.includes(child.urlpara))
 
                         if (currentValues.includes(child.urlpara)) {
                           console.log('removing:', child.urlpara)
+                          console.log(index, currentValues)
                           index = currentValues.indexOf(child.urlpara)
                           currentValues.splice(index, 1)
 
-                          this.params.set(filterItem.urlpara, currentValues.join(','))
+                          params.set(`${filterItem.urlpara}_${filterItem.id}`, currentValues.join(','))
                         }
-                        if (this.params.get(filterItem.urlpara) === '') {
+                        if (params.get(filterItem.urlpara) === '') {
                           console.log('deleting:', filterItem.urlpara)
-                          this.params.delete(filterItem.urlpara)
+                          params.delete(filterItem.urlpara)
                         }
                       }
                     }
                   })
-
-                  window.history.pushState({}, '', `${this.url.pathname}?${this.params.toString()}`)
-                }
-                
-                // get selected filter from url params
-                if (filterItem && this.params.has(filterItem.urlpara)) {
-                  if (filterItem.children && filterItem.children.length > 0) {
-                    filterItem.children.forEach(child => {
-                      selectedFilter = String(this.params.get(filterItem.urlpara)?.split(','))
-                      if (selectedFilter?.includes(child.urlpara)) {
-                        child.selected = true
-                      }
-                    })
-                  }
+                  
+                  console.log('currentParams', filterItem.urlpara, params.get(filterItem.urlpara)?.split(','))
+                  self.history.pushState({}, '', `${url.pathname}?${params.toString()}`)
                 }
               })
-
               return { ...json, numberOfOffers }
             })).get(request)
         },
@@ -213,5 +182,49 @@ export default class WithFacet extends Shadow() {
 
   disconnectedCallback () {
     this.removeEventListener('request-with-facet', this.requestWithFacetListener)
+  }
+
+  constructFilterItem (event) {
+    const filterItem = event.detail?.wrapper?.filterItem
+
+    return filterItem
+      ? `{
+        "children": [
+          ${filterItem.children.map(child => {
+            const count = child.count ? `(${child.count})` : ''
+            const label = count ? `${child.label} ${count}` : child.label
+            const hasSameLabel = label.trim() === event.detail?.target.label.trim()
+            const isCheckedNullOrUndefined = event.detail?.target.checked === null || event.detail?.target.checked === undefined
+
+            return `{
+              ${child.count ? `"count": ${child.count},` : ''}
+              ${child.eTag ? `"eTag": "${child.eTag.replace(/"/g, '\\"')}",` : ''}
+              "hasChilds": ${child.hasChilds},
+              "id": "${child.id}",
+              "label": "${child.label}",
+              ${child.partitionKey ? `"partitionKey": "${child.partitionKey}",` : ''}
+              ${child.rowKey ? `"rowKey": "${child.rowKey}",` : ''}
+              "selected": ${hasSameLabel ? isCheckedNullOrUndefined ? child.selected : event.detail.target.checked : child.selected},
+              ${child.sort ? `"sort": ${child.sort},` : ''}
+              ${child.timestamp ? `"timestamp": "${child.timestamp}",` : ''}
+              ${child.typ ? `"typ": "${child.typ}",` : ''}
+              "urlpara": "${child.urlpara}"
+            }`
+          })}
+        ],
+        ${filterItem.disabled ? `"disabled": ${filterItem.disabled},` : ''}
+        ${filterItem.eTag ? `"eTag": "${filterItem.eTag.replace(/"/g, '\\"')}",` : ''}
+        ${filterItem.hasChilds ? `"hasChilds": ${filterItem.hasChilds},` : ''}
+        ${filterItem.id ? `"id": "${filterItem.id}",` : ''}
+        ${filterItem.label ? `"label": "${filterItem.label}",` : ''}
+        ${filterItem.options ? `"options": ${filterItem.options},` : ''}
+        ${filterItem.partitionKey ? `"partitionKey": "${filterItem.partitionKey}",` : ''}
+        ${filterItem.rowKey ? `"rowKey": "${filterItem.rowKey}",` : ''}
+        ${filterItem.sort ? `"sort": ${filterItem.sort},` : ''}
+        ${filterItem.timestamp ? `"timestamp": "${filterItem.timestamp}",` : ''}
+        ${filterItem.typ ? `"typ": "${filterItem.typ}",` : ''}
+        "visible": ${filterItem.visible || true}
+      }`
+      : ''
   }
 }
--- src/es/components/molecules/filterCategories/FilterCategories.js
+++ src/es/components/molecules/filterCategories/FilterCategories.js
@@ -17,7 +17,7 @@ export default class FilterCategories extends Shadow() {
 
     this.withFacetEventListener = event => this.renderHTML(event.detail.fetch)
 
-    this.html = this.mainNav
+    // this.html = this.mainNav
   }
 
   connectedCallback () {
@@ -80,99 +80,107 @@ export default class FilterCategories extends Shadow() {
           name: 'm-dialog'
         }
       ])
-    ]).then(([response]) => {
-      // console.log('response (FilterCategories.js)', response)
-      const filterData = response.filters
-      // Backend should give us the data sorted, but in case it doesn't, we can sort it here
-      // filterData.sort((a, b) => a.sort - b.sort);
-      let numberOfOffers = 0
+    ]).then(() => {
+      fetch.then(response => {
+        // console.log('response (FilterCategories.js)', response)
+        const filterData = response.filters
+        // Backend should give us the data sorted, but in case it doesn't, we can sort it here
+        // filterData.sort((a, b) => a.sort - b.sort);
+        let numberOfOffers = 0
 
-      filterData.forEach((filterItem, i) => {
-        let childItems = ''
-        const subNav = []
-        if (filterItem.children && filterItem.children.length > 0 && filterItem.visible) {
-          filterItem.children.forEach(child => {
-            if (child.selected) {
-              childItems += child.label + ', '
-            }
-            const count = child.count ? `(${child.count})` : ''
-            const disabled = child.disabled || child.count === 0 ? 'disabled' : ''
-            const checked = child.selected ? 'checked' : ''
-            const visible = child.visible ? 'visible' : ''
-            const div = document.createElement('div')
-            div.innerHTML = /* html */`
-              <mdx-component mutation-callback-event-name="request-with-facet">
-                <mdx-checkbox ${checked} ${disabled} ${visible} variant="no-border" label="${child.label} ${count}"></mdx-checkbox>
-              </mdx-component>
+        this.html = ''
+        filterData.forEach((filterItem, i) => {
+          let childItems = ''
+          const subNav = []
+          
+          if (filterItem.children && filterItem.children.length > 0 && filterItem.visible) {
+            filterItem.children.forEach(child => {
+              if (child.selected) {
+                childItems += child.label + ', '
+              }
+              const count = child.count ? `(${child.count})` : ''
+              const disabled = child.disabled || child.count === 0 ? 'disabled' : ''
+              const checked = child.selected ? 'checked' : ''
+              const visible = child.visible ? 'visible' : ''
+              const div = document.createElement('div')
+              div.innerHTML = /* html */`
+                <mdx-component mutation-callback-event-name="request-with-facet">
+                  <mdx-checkbox ${checked} ${disabled} ${visible} variant="no-border" label="${child.label} ${count}"></mdx-checkbox>
+                </mdx-component>
+              `
+              // @ts-ignore
+              div.children[0].filterItem = filterItem
+              subNav.push(div.children[0])
+
+              if (child.selected && child.count > 0) {
+                numberOfOffers += child.count
+              }
+            })
+
+            this.html = this.mainNav
+          }
+
+          let resetButton = ''
+          if (this.hasAttribute('translation-key-reset')) {
+            resetButton = /* html */`
+              <p class="reset-link"><a-button namespace="button-transparent-">${this.getAttribute('translation-key-reset')}<a-icon-mdx class="icon-right" icon-name="RotateLeft" size="1em"></a-icon-mdx></a-button></p>
             `
-            // @ts-ignore
-            div.children[0].filterItem = filterItem
-            subNav.push(div.children[0])
+          }
 
-            if (child.selected && child.count > 0) {
-              numberOfOffers += child.count
-            }
-          })
-        }
+          if (this.mainNav.children[i]?.getAttribute('id') === filterItem.id) {
+            const targetNode = this.mainNav.children[i].root.querySelector('.sub-level')
+            targetNode.innerHTML = ''
+            subNav.forEach(node => targetNode.appendChild(node))
+          } else {
+            if (filterItem.visible === false || filterItem.children?.length === 0) return
 
-        let resetButton = ''
-        if (this.hasAttribute('translation-key-reset')) {
-          resetButton = /* html */`
-            <p class="reset-link"><a-button namespace="button-transparent-">${this.getAttribute('translation-key-reset')}<a-icon-mdx class="icon-right" icon-name="RotateLeft" size="1em"></a-icon-mdx></a-button></p>
-          `
-        }
-
-        if (this.mainNav.children[i]?.getAttribute('id') === filterItem.id) {
-          const targetNode = this.mainNav.children[i].root.querySelector('.sub-level')
-          targetNode.innerHTML = ''
-          subNav.forEach(node => targetNode.appendChild(node))
-        } else {
-          if (filterItem.visible === false || filterItem.children.length === 0) return
-
-          const navLevelItem = /* html */ `
-            <m-dialog id="${filterItem.id}" namespace="dialog-left-slide-in-without-background-" show-event-name="dialog-open-${filterItem.id}" close-event-name="backdrop-clicked">
-              <div class="container dialog-header" tabindex="0">
-                <a-button id="close-back">
-                  <a-icon-mdx icon-name="ChevronLeft" size="2em" id="close"></a-icon-mdx>
-                </a-button>
-                <h3>${filterItem.label}</h3>
-                <a-button request-event-name="backdrop-clicked" id="close">
-                  <a-icon-mdx icon-name="Plus" size="2em" rotate="45deg" no-hover-transform></a-icon-mdx>
-                </a-button>
-              </div>
-              <div class="dialog-content">
-                ${this.hasAttribute('translation-key-reset') ? resetButton : ''}
-                <div class="sub-level ${this.hasAttribute('translation-key-reset') ? 'margin-bottom' : 'margin-top-bottom'}"></div>       
-              </div>
-              <div class="container dialog-footer">
-                <a-button id="close" namespace="button-secondary-" no-pointer-events request-event-name="backdrop-clicked">${this.getAttribute('translation-key-close')}</a-button>
-                <a-button id="close" class="button-show-all-offers" namespace="button-primary-" no-pointer-events request-event-name="backdrop-clicked">${numberOfOffers > 0 ? `(${numberOfOffers}) ` : ''}${this.getAttribute('translation-key-cta')}</a-button>
-              </div>
-              <ks-m-nav-level-item namespace="nav-level-item-default-" id="show-modal">
-                <div class="wrap">
-                  <span class="text">${filterItem.label}</span>
-                  <span class="additional">${childItems.slice(0, -2)}</span>
+            const navLevelItem = /* html */ `
+              <m-dialog id="${filterItem.id}" namespace="dialog-left-slide-in-without-background-" show-event-name="dialog-open-${filterItem.id}" close-event-name="backdrop-clicked">
+                <div class="container dialog-header" tabindex="0">
+                  <a-button id="close-back">
+                    <a-icon-mdx icon-name="ChevronLeft" size="2em" id="close"></a-icon-mdx>
+                  </a-button>
+                  <h3>${filterItem.label}</h3>
+                  <a-button request-event-name="backdrop-clicked" id="close">
+                    <a-icon-mdx icon-name="Plus" size="2em" rotate="45deg" no-hover-transform></a-icon-mdx>
+                  </a-button>
                 </div>
-                <a-icon-mdx namespace="icon-link-list-" icon-name="ChevronRight" size="1.5em" rotate="0" class="icon-right"></a-icon-mdx>
-              </ks-m-nav-level-item>
-            </m-dialog>
-          `
-          const div = document.createElement('div')
+                <div class="dialog-content">
+                  ${this.hasAttribute('translation-key-reset') ? resetButton : ''}
+                  <div class="sub-level ${this.hasAttribute('translation-key-reset') ? 'margin-bottom' : 'margin-top-bottom'}"></div>       
+                </div>
+                <div class="container dialog-footer">
+                  <a-button id="close" namespace="button-secondary-" no-pointer-events request-event-name="backdrop-clicked">${this.getAttribute('translation-key-close')}</a-button>
+                  <a-button id="close" class="button-show-all-offers" namespace="button-primary-" no-pointer-events request-event-name="backdrop-clicked">${numberOfOffers > 0 ? `(${numberOfOffers}) ` : ''}${this.getAttribute('translation-key-cta')}</a-button>
+                </div>
+                <ks-m-nav-level-item namespace="nav-level-item-default-" id="show-modal">
+                  <div class="wrap">
+                    <span class="text">${filterItem.label}</span>
+                    <span class="additional">${childItems.slice(0, -2)}</span>
+                  </div>
+                  <a-icon-mdx namespace="icon-link-list-" icon-name="ChevronRight" size="1.5em" rotate="0" class="icon-right"></a-icon-mdx>
+                </ks-m-nav-level-item>
+              </m-dialog>
+            `
+            const div = document.createElement('div')
 
-          div.innerHTML = navLevelItem
-          // @ts-ignore
-          const targetNode = div.children[0].root.querySelector('.sub-level')
-          subNav.forEach(node => targetNode.appendChild(node))
-          this.mainNav.appendChild(div.children[0])
-        }
+            div.innerHTML = navLevelItem
+            // @ts-ignore
+            const targetNode = div.children[0].root.querySelector('.sub-level')
+            subNav.forEach(node => targetNode.appendChild(node))
+            this.mainNav.appendChild(div.children[0])
+          }
+        })
       })
     })
   }
 
   get mainNav () {
     if (this.root.querySelector('.main-level')) return this.root.querySelector('.main-level')
+    
     const mainNav = document.createElement('div')
     mainNav.setAttribute('class', 'main-level')
+
     return mainNav
   }
 }
