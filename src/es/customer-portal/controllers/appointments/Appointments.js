0826b5081dcb056eaf864cf002a691a0218a90ae
edmgb <evangelos.dinopoulos@mgb.ch>
2024-04-15 19:26:18 +0200

    refactor

--- src/es/customer-portal/controllers/appointments/Appointments.js
+++ src/es/customer-portal/controllers/appointments/Appointments.js
@@ -1,17 +1,19 @@
 // @ts-check
+import { actionType } from '../../helpers/Mapping.js'
 import { makeUniqueCourseId } from '../../helpers/Shared.js'
 
-/* global self */
 /* global AbortController */
 /* global CustomEvent */
 /* global fetch */
 /* global HTMLElement */
+/* global self */
 
 /**
-* @export
-* @class Appointments
-* @type {CustomElementConstructor}
-*/
+ * This class is responsible for managing the appointments functionality in the customer portal.
+ * @example ./pages/Appointments.js
+ * @class Appointments
+ * @type {CustomElementConstructor}
+ */
 export default class Appointments extends HTMLElement {
   constructor () {
     super()
@@ -35,17 +37,19 @@ export default class Appointments extends HTMLElement {
     this.removeEventListener(this.getAttribute('request-subscription-course-appointment-booking') || 'request-subscription-course-appointment-booking', this.requestSubscriptionCourseAppointmentBookingListener)
   }
 
-  // LIST ALL
+  /**
+   * List all Course Appointments
+   * @param {CustomEventInit} event
+   */
   requestSubscriptionCourseAppointmentsListener = async (event) => {
+    // TODO: When use dropdown send this data
+    // subscriptionId
+    // subscriptionType
+    // userId
     if (this.abortControllerSubscriptionCourseAppointments) this.abortControllerSubscriptionCourseAppointments.abort()
     this.abortControllerSubscriptionCourseAppointments = new AbortController()
-
-    // when use dropdown send this data
-    /*
-      subscriptionId
-      subscriptionType
-      userId
-     */
+    // @ts-ignore
+    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointments}`
     const { subscriptionType, subscriptionId } = event.detail
     const { userId } = this.dataset
     const data = {
@@ -53,9 +57,7 @@ export default class Appointments extends HTMLElement {
       subscriptionType,
       subscriptionId
     }
-    const fetchOptions = this.fetchOptions(data, this.abortControllerSubscriptionCourseAppointments)
-    // @ts-ignore
-    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointments}`
+    const fetchOptions = this.fetchPOSTOptions(data, this.abortControllerSubscriptionCourseAppointments)
     this.dispatchEvent(new CustomEvent(this.getAttribute('update-subscription-course-appointments') || 'update-subscription-course-appointments', {
       detail: {
         fetch: fetch(endpoint, fetchOptions).then(async response => {
@@ -68,11 +70,17 @@ export default class Appointments extends HTMLElement {
     }))
   }
 
-  // MAKE BOOKING
+  /**
+   * Make Course Booking
+   * @param {CustomEventInit} event
+   */
   requestSubscriptionCourseAppointmentBookingListener = async (event) => {
     if (this.abortControllerSubscriptionCourseAppointmentBooking) this.abortControllerSubscriptionCourseAppointmentBooking.abort()
     this.abortControllerSubscriptionCourseAppointmentBooking = new AbortController()
+    // @ts-ignore
+    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointmentBooking}`
     const tags = JSON.parse(event.detail.tags)
+    const courseId = makeUniqueCourseId(tags[0])
     const data = {
       courseAppointmentDate: tags[0].courseAppointmentDate,
       courseAppointmentTimeFrom: tags[0].courseAppointmentTimeFrom,
@@ -82,17 +90,14 @@ export default class Appointments extends HTMLElement {
       subscriptionType: tags[1].subscriptionType,
       userId: this.dataset.userId
     }
-    const courseId = makeUniqueCourseId(tags[0])
-    const fetchOptions = this.fetchOptions(data, this.abortControllerSubscriptionCourseAppointmentBooking)
-    // @ts-ignore
-    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointmentBooking}`
+    const fetchOptions = this.fetchPOSTOptions(data, this.abortControllerSubscriptionCourseAppointmentBooking)
     this.dispatchEvent(new CustomEvent(this.getAttribute('update-subscription-course-appointment-booking') || 'update-subscription-course-appointment-booking', {
       detail: {
         fetch: fetch(endpoint, fetchOptions).then(async response => {
           if (response.status >= 200 && response.status <= 299) return await response.json()
         }),
         id: courseId,
-        type: 'booking'
+        type: actionType.booking
       },
       bubbles: true,
       cancelable: true,
@@ -100,11 +105,17 @@ export default class Appointments extends HTMLElement {
     }))
   }
 
-  // GET DETAILS
+  /**
+   * Get Course Detail
+   * @param {CustomEventInit} event
+   */
   requestSubscriptionCourseAppointmentDetailListener = async (event) => {
     if (this.abortControllerSubscriptionCourseAppointmentDetail) this.abortControllerSubscriptionCourseAppointmentDetail.abort()
     this.abortControllerSubscriptionCourseAppointmentDetail = new AbortController()
+    // @ts-ignore
+    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointmentDetail}`
     const tags = JSON.parse(event.detail.tags)
+    const courseId = makeUniqueCourseId(tags[0])
     const data = {
       courseAppointmentDate: tags[0].courseAppointmentDate,
       courseAppointmentTimeFrom: tags[0].courseAppointmentTimeFrom,
@@ -112,13 +123,10 @@ export default class Appointments extends HTMLElement {
       courseType: tags[0].courseType,
       subscriptionId: tags[1].subscriptionId,
       subscriptionType: tags[1].subscriptionType,
-      userId: this.dataset.userId || ''
+      userId: this.dataset.userId
     }
     const type = tags[2] ? tags[2].type : ''
-    const courseId = makeUniqueCourseId(tags[0])
-    const fetchOptions = this.fetchOptions(data, this.abortControllerSubscriptionCourseAppointmentDetail)
-    // @ts-ignore
-    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointmentDetail}`
+    const fetchOptions = this.fetchPOSTOptions(data, this.abortControllerSubscriptionCourseAppointmentDetail)
     this.dispatchEvent(new CustomEvent(this.getAttribute('update-subscription-course-appointment-detail') || 'update-subscription-course-appointment-detail', {
       detail: {
         fetch: fetch(endpoint, fetchOptions).then(async response => {
@@ -133,11 +141,17 @@ export default class Appointments extends HTMLElement {
     }))
   }
 
-  // REVERSAL
+  /**
+   * Make Course Reversal
+   * @param {CustomEventInit} event
+   */
   requestSubscriptionCourseAppointmentReversalListener = async (event) => {
     if (this.abortControllerSubscriptionCourseAppointmentReversalListener) this.abortControllerSubscriptionCourseAppointmentReversalListener.abort()
     this.abortControllerSubscriptionCourseAppointmentReversalListener = new AbortController()
+    // @ts-ignore
+    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointmentReversal}`
     const tags = JSON.parse(event.detail.tags)
+    const courseId = makeUniqueCourseId(tags[0])
     const data = {
       courseAppointmentDate: tags[0].courseAppointmentDate,
       courseAppointmentTimeFrom: tags[0].courseAppointmentTimeFrom,
@@ -145,19 +159,16 @@ export default class Appointments extends HTMLElement {
       courseType: tags[0].courseType,
       subscriptionId: tags[1].subscriptionId,
       subscriptionType: tags[1].subscriptionType,
-      userId: this.dataset.userId || ''
+      userId: this.dataset.userId
     }
-    const courseId = makeUniqueCourseId(tags[0])
-    const fetchOptions = this.fetchOptions(data, this.abortControllerSubscriptionCourseAppointmentReversalListener)
-    // @ts-ignore
-    const endpoint = `${self.Environment.getApiBaseUrl('customer-portal').apiSubscriptionCourseAppointmentReversal}`
+    const fetchOptions = this.fetchPOSTOptions(data, this.abortControllerSubscriptionCourseAppointmentReversalListener)
     this.dispatchEvent(new CustomEvent(this.getAttribute('update-subscription-course-appointment-reversal') || 'update-subscription-course-appointment-reversal', {
       detail: {
         fetch: fetch(endpoint, fetchOptions).then(async response => {
           if (response.status >= 200 && response.status <= 299) return await response.json()
         }),
         id: courseId,
-        type: 'reversal'
+        type: actionType.reversal
       },
       bubbles: true,
       cancelable: true,
@@ -171,7 +182,7 @@ export default class Appointments extends HTMLElement {
    * @param {AbortController} abortController - Abort Fetch requests
    * @returns {Object} An object is being returned to use as option object for api fetch
    */
-  fetchOptions (data, abortController) {
+  fetchPOSTOptions (data, abortController) {
     return {
       method: 'POST',
       headers: {
