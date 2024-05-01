3fb156cf952378d360eb796e5c00470c5f52db57
sturudic <srdan.turudic@emea.merkleinc.com>
2024-05-02 01:49:57 +0200

    feat(form): MIDUWEB-653 add form component

---
+++ src/es/components/organisms/form/Form.js
@@ -0,0 +1,37 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+
+/**
+* @export
+* @class Form
+* @type {CustomElementConstructor}
+*/
+export default class Form extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+  }
+
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS()
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
+  }
+
+  /**
+   * renders the css
+   */
+  renderCSS () {
+    this.css = /* css */`
+      :host h1 {
+        color: red;
+      }
+    `
+  }
+}
