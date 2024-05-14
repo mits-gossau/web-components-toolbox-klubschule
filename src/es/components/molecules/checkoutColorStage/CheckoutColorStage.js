aa59931640e426dd49da9dc0953d6707870a7731
Thomas Deser <thomas.deser@merkle.com>
2024-05-14 11:36:30 +0200

    feat: add checkout color stage with back link

---
+++ src/es/components/molecules/checkoutColorStage/CheckoutColorStage.js
@@ -0,0 +1,112 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+
+/**
+* @export
+* @class Contact
+* @type {CustomElementConstructor}
+*/
+export default class Contact extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, mode: 'false', ...options }, ...args)
+
+    this.backLinkListener = this.backLinkListener.bind(this)
+  }
+
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS()
+    if (this.shouldRenderHTML()) {
+      this.renderHTML().then(() => {
+        if (!this.backButton) {
+          this.backButton = this.root.querySelector('o-grid').shadowRoot.querySelector('.back-button')
+          this.backButton.addEventListener('click', this.backLinkListener)
+        }
+      })
+    }
+  }
+
+  disconnectedCallback () {
+    this.backButton.removeEvenetListener('click', this.backLinkListener)
+  }
+
+  backLinkListener (event) {
+    event.preventDefault()
+    window.history.back()
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderHTML () {
+    return !this.grid
+  }
+
+  /**
+   * renders the css
+   */
+  renderCSS () {
+    this.css = /* css */`
+      :host {
+        width: 100% !important;
+        margin-left: 0 !important;
+        margin-right: 0 !important;
+        background-color: var(--mdx-sys-color-primary-default);
+      }
+    `
+  }
+
+  /**
+   * Render HTML
+   * @returns Promise<void>
+   */
+  renderHTML () {
+    return this.fetchModules([
+      {
+        path: `${this.importMetaUrl}../../web-components-toolbox/src/es/components/organisms/grid/Grid.js`,
+        name: 'o-grid'
+      },
+      {
+        path: `${this.importMetaUrl}../../atoms/heading/Heading.js`,
+        name: 'ks-a-heading'
+      }
+    ]).then(() => {
+      this.html = /* HTML */ `
+        <o-grid 
+            namespace="grid-2columns-content-stage-" 
+            first-container-vertical 
+            first-column-with="66%" 
+            width="100%"
+        >
+            <div class="stage-content">
+                <p class="topline link-underline">
+                    ${(window.history.length > 1)
+                        ? /* html */`
+                            <a class="back-button" href="#">
+                                <a-icon-mdx icon-name="ArrowLeft" size="1em"></a-icon-mdx>
+                                <span>${this.getAttribute('back-label')}</span>
+                            </a>`
+                        : ''
+                    }
+                </p>
+                <ks-a-heading tag="h1" color="white" content-stage>${this.getAttribute('title')}</ks-a-heading>
+            </div>
+        </o-grid>
+    `
+    })
+  }
+
+  get grid () {
+    return this.root.querySelector('o-grid')
+  }
+}
