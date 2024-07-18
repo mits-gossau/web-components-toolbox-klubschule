a3ce8201140b454bf7ad6150c70c336f414f9a2b
Alexander Schmidt <alexander@schmidt.eco>
2024-07-18 13:22:18 +0200

    refactor(TileFactory.js): improve condition for clearing HTML content based on data.ppage and this.lastResponsePPage

--- src/es/components/molecules/tileFactory/TileFactory.js
+++ src/es/components/molecules/tileFactory/TileFactory.js
@@ -121,7 +121,8 @@ export default class TileFactory extends Shadow() {
       setTimeout(() => {
         // remove loading component
         this.root.querySelector('.mdx-loading')?.remove()
-        if (data.ppage === 1 || (data.ppage === -1 && this.lastResponsePPage === -1)) this.html = ''
+        console.log(data.ppage, this.lastResponsePPage)
+        if (data.ppage === 1 || (data.ppage === -1 && [-1, 1, 0].includes(this.lastResponsePPage))) this.html = ''
         if (!data) {
           this.html = `<span class=error><a-translation data-trans-key="${this.getAttribute('error-text') ?? 'Search.Error'}"></a-translation></span>`
           return
