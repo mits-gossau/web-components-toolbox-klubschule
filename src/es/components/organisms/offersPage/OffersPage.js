84ee9c0f28f80ca8b08a5633fff89b37128d9faa
Alexander Schmidt <alexander@schmidt.eco>
2024-09-20 09:59:19 +0200

    chore(OffersPage.js): add conditional 'any-key-listener' attribute based on 'with-auto-complete' attribute presence chore(Angebotsliste-Suchergebnis.html): add 'with-auto-complete' attribute to component

--- src/es/components/organisms/offersPage/OffersPage.js
+++ src/es/components/organisms/offersPage/OffersPage.js
@@ -769,6 +769,7 @@ export default class OffersPage extends Shadow() {
               type="search"
               answer-event-name="search-change"
               delete-listener
+              ${this.hasAttribute('with-auto-complete') ? 'any-key-listener' : ''}
               search
               autocomplete="off"
             >
