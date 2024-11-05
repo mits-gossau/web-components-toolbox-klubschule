a0ce157a8c74c0e5c285a400486065f73b386125
Weedshaker <weedshaker@gmail.com>
2024-11-05 10:22:10 +0100

    added attribute

--- src/es/components/organisms/offersPage/OffersPage.js
+++ src/es/components/organisms/offersPage/OffersPage.js
@@ -656,7 +656,8 @@ export default class OffersPage extends Shadow() {
                 ${this.isWishList ? ' is-wish-list' : ''}
                 ${this.hasAttribute('is-info-events') ? ` is-info-events loading-text="${this.getTranslation('CourseList.LabelLoaderInfoEvent')}" style="width:100%"` : ''}
                 ${this.hasAttribute('with-facet-target') ? ' with-facet-target' : ''}
-                ${this.hasAttribute('error-text') ? `error-text="${this.getAttribute('error-text')}"` : ''}
+                ${this.hasAttribute('no-partner-search') ? ' no-partner-search' : ''}
+                ${this.hasAttribute('error-text') ? ` error-text="${this.getAttribute('error-text')}"` : ''}
               >
                 ${this.hiddenSections.reduce((acc, hiddenSection) => (acc + hiddenSection.outerHTML), '')}
               </ks-m-tile-factory>
@@ -723,6 +724,7 @@ export default class OffersPage extends Shadow() {
           <section>
             <div col-lg="12" col-md="12" col-sm="12">
               <ks-m-content-factory
+                ${this.hasAttribute('no-partner-search') ? ' no-partner-search' : ''}
                 ${this.hasAttribute('error-text') ? `error-text="${this.getAttribute('error-text')}"` : ''}
               >
                 ${this.hiddenSections.reduce((acc, hiddenSection) => (acc + hiddenSection.outerHTML), '')}
