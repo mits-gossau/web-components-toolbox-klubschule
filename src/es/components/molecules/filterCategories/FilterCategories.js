30261ba10b21381e168d3153fa85200fdd3e6c89
Alexander Schmidt <alexander@schmidt.eco>
2024-11-06 14:34:09 +0100

    feat(FilterCategories.js): improve condition to hide navLevelItem [MIDUWEB-1662]

--- src/es/components/molecules/filterCategories/FilterCategories.js
+++ src/es/components/molecules/filterCategories/FilterCategories.js
@@ -306,7 +306,7 @@ export default class FilterCategories extends Shadow() {
     `
 
     div.innerHTML = /* html */`
-      <m-dialog ${hidden !== '' ? 'style="display:none"' : ''} id="${filterIdPrefix + filterItem.id}" ${shouldRemainOpen ? 'open' : ''} namespace="dialog-left-slide-in-without-background-" show-event-name="dialog-open-${filterItem.id}" close-event-name="backdrop-clicked">
+      <m-dialog ${hidden ? 'style="display:none"' : ''} id="${filterIdPrefix + filterItem.id}" ${shouldRemainOpen ? 'open' : ''} namespace="dialog-left-slide-in-without-background-" show-event-name="dialog-open-${filterItem.id}" close-event-name="backdrop-clicked">
         <div class="container dialog-header" tabindex="0">
           <a-button id="close-back">
             <a-icon-mdx icon-name="ChevronLeft" size="2em" id="close"></a-icon-mdx>
