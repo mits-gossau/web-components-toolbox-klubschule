3b8860325dba149b181c8abbd4604a30a4746252
Alexander Schmidt <alexander@schmidt.eco>
2024-11-14 09:07:46 +0100

    save

--- src/es/components/organisms/tileList/TileList.js
+++ src/es/components/organisms/tileList/TileList.js
@@ -301,6 +301,7 @@ export default class TileList extends Shadow() {
     const warnMandatory = 'data attribute requires: '
     if (!data) return console.error('Data json attribute is missing or corrupted!', this)
     this.data = data
+    console.log(data)
     // don't wait for fetchModules to resolve if using "shouldRenderHTML" checks for this.badge it has to be sync
     // NOTE: the replace ".replace(/'/g, '’')" avoids the dom to close the attribute string unexpectedly. This replace is also ISO 10646 conform as the character ’ (U+2019) is the preferred character for apostrophe. See: https://www.cl.cam.ac.uk/~mgk25/ucs/quotes.html + https://www.compart.com/de/unicode/U+2019
     this.html = /* html */`
@@ -318,6 +319,7 @@ export default class TileList extends Shadow() {
                     "item_name": "${data.title || data.bezeichnung || 'No Title'}",                
                     "item_id": "${data.kurs_typ}_${data.kurs_id}",
                     "price": ${data.price?.price || data.preis_total || 0},
+                    ${data.spartename?.length ? `"item_category": "${data.spartename[0]}",` : ''}
                     "quantity": 1,
                     ${data.location?.center ? `"item_variant": "${data.location.center}",` : ''}
                     "currency": "CHF"
---
+++ src/es/components/pages/generator/https---dev-klubschule-ch-kreativitaet-zeichnen-und-malen.html.spec.js
@@ -0,0 +1,28 @@
+
+    const { test, expect } = require('@playwright/test')
+
+    const dir = process.cwd().split('/').pop().split('-')
+    const PROJECT_NAME = dir.splice(3)[0]
+    const WAITING_TIMEOUT = 200
+
+    test('https---dev-klubschule-ch-kreativitaet-zeichnen-und-malen.html', async ({ page, browserName }) => {
+      await page.goto('./src/es/components/web-components-toolbox/docs/TemplateMiduweb.html?rootFolder=src&css=./src/css/variablesCustomKlubschule.css&login=./src/es/components/molecules/login/default-/default-.html&logo=./src/es/components/atoms/logo/default-/default-.html&nav=./src/es/components/web-components-toolbox/src/es/components/molecules/multiLevelNavigation/default-/default-.html&footer=./src/es/components/organisms/footer/default-/default-.html&content=./src/es/components/pages/generator/https---dev-klubschule-ch-kreativitaet-zeichnen-und-malen.html')
+      const demoPage = await page.waitForSelector('body')
+      const wcLoaded = await demoPage.getAttribute('wc-config-load')
+      console.log('wait for wc-load')
+      if (wcLoaded) {
+        console.log('loaded')
+        await page.evaluate(() => document.fonts.ready)
+        await page.waitForTimeout(WAITING_TIMEOUT)
+        await page.evaluate(() => window.scrollTo(0, 999999999))
+        await page.waitForTimeout(WAITING_TIMEOUT)
+        await page.evaluate(() => window.scrollTo(0, 999999999))
+        await page.waitForTimeout(WAITING_TIMEOUT)
+        await page.evaluate(() => window.scrollTo(0, 999999999))
+        await page.waitForTimeout(WAITING_TIMEOUT)
+        await page.evaluate(() => window.scrollTo(0, 0))
+        await page.waitForTimeout(WAITING_TIMEOUT)
+        expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`${PROJECT_NAME || 'snapshot'}.png`)
+      }
+    })
+  
