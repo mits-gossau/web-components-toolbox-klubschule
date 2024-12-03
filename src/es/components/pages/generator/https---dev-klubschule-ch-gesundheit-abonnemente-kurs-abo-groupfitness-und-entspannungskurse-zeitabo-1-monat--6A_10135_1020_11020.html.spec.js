73e2ed3fe584c487577c79ea3ddf839fb73e2f36
Alexander Schmidt <alexander@schmidt.eco>
2024-12-03 08:35:50 +0100

    feat(generator): rename and update HTML files for new course offerings and remove outdated content chore(tests): rename and update test files to reflect new course structure and remove obsolete tests
    chore: remove obsolete HTML and test files for various courses to clean up the codebase and improve maintainability
    
    chore: remove obsolete HTML and test files for the language generator page to clean up the codebase and improve maintainability
    
    chore: remove outdated HTML and test files for various courses to clean up the codebase and improve maintainability
    
    chore: remove outdated course HTML and spec files for group fitness and mindfulness training
    
    This commit deletes obsolete HTML and test files related to the
    "Groupfitness- und Entspannungskurse" and "Achtsamkeitstraining"
    courses, as they are no longer relevant to the current offerings.
    Cleaning up these files helps maintain a tidy codebase and
    prevents confusion regarding available courses.
    
    chore: remove outdated HTML and test files for English and German courses to clean up the codebase and improve maintainability
    
    chore: remove unused HTML and test files related to the "Bauch Beine Po" and "Kreativität" courses to clean up the codebase and improve maintainability
    
    chore(generator): remove obsolete HTML and spec files for various courses and services to clean up the project and improve maintainability

--- src/es/components/pages/generator/https---dev-klubschule-ch-gesundheit-abonnemente-kurs-abo-groupfitness-und-entspannungskurse-zeitabo-1-monat--6A_10135_1020_11020.html.spec.js
+++ src/es/components/pages/generator/https---dev-klubschule-ch-gesundheit-abonnemente-kurs-abo-groupfitness-und-entspannungskurse-zeitabo-1-monat--6A_10135_1020_11020.html.spec.js
@@ -5,8 +5,8 @@
     const PROJECT_NAME = dir.splice(3)[0]
     const WAITING_TIMEOUT = 200
 
-    test('https---int-klubschule-ch-kurs-abo-groupfitness-und-entspannungskurse-zeitabo-12-mon--6A_10132_1020_11020-q-abo-groupfitness-und-entspannungskurse-zeitabo.html', async ({ page, browserName }) => {
-      await page.goto('./src/es/components/web-components-toolbox/docs/TemplateMiduweb.html?rootFolder=src&css=./src/css/variablesCustomKlubschule.css&login=./src/es/components/molecules/login/default-/default-.html&logo=./src/es/components/atoms/logo/default-/default-.html&nav=./src/es/components/web-components-toolbox/src/es/components/molecules/multiLevelNavigation/default-/default-.html&footer=./src/es/components/organisms/footer/default-/default-.html&content=./src/es/components/pages/generator/https---int-klubschule-ch-kurs-abo-groupfitness-und-entspannungskurse-zeitabo-12-mon--6A_10132_1020_11020-q-abo-groupfitness-und-entspannungskurse-zeitabo.html')
+    test('https---dev-klubschule-ch-gesundheit-abonnemente-kurs-abo-groupfitness-und-entspannungskurse-zeitabo-1-monat--6A_10135_1020_11020.html', async ({ page, browserName }) => {
+      await page.goto('./src/es/components/web-components-toolbox/docs/TemplateMiduweb.html?rootFolder=src&css=./src/css/variablesCustomKlubschule.css&login=./src/es/components/molecules/login/default-/default-.html&logo=./src/es/components/atoms/logo/default-/default-.html&nav=./src/es/components/web-components-toolbox/src/es/components/molecules/multiLevelNavigation/default-/default-.html&footer=./src/es/components/organisms/footer/default-/default-.html&content=./src/es/components/pages/generator/https---dev-klubschule-ch-gesundheit-abonnemente-kurs-abo-groupfitness-und-entspannungskurse-zeitabo-1-monat--6A_10135_1020_11020.html')
       const demoPage = await page.waitForSelector('body')
       const wcLoaded = await demoPage.getAttribute('wc-config-load')
       console.log('wait for wc-load')
