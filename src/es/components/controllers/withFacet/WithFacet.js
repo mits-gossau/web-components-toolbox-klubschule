82c58b8fa3d5d7f4e0ec963fc2c6fa907eb5f225
Alexander Schmidt <alexander@schmidt.eco>
2025-01-14 15:19:33 +0100

    fix(WithFacet.js): introduce gtm tracking for filters again [MIDUWEB-1657]

--- src/es/components/controllers/withFacet/WithFacet.js
+++ src/es/components/controllers/withFacet/WithFacet.js
@@ -191,6 +191,13 @@ export default class WithFacet extends WebWorker() {
           this.updateURLParam(filterKey, filterValue, false)
         }
 
+        // GTM Tracking of Filters
+        if (event.detail?.target?.checked) this.dataLayerPush({
+          'event': 'filterSelection',
+          'filterName': event.detail.target.label, //the name of the clicked filter.
+          'filterCategory': filterGroupName.attributes?.label ? filterGroupName.attributes.label.value : filterGroupName.label, //the category that this filter belongs to - IF there is one, if not we can remove this key
+        })
+        
         const result = await this.webWorker(WithFacet.updateFilters, currentCompleteFilterObj, filterKey, filterValue, false, true, null, false, isTree)
         currentCompleteFilterObj = result[0]
         currentRequestObj.filter = [...result[1], ...initialFilter.filter(filter => !result[1].find(resultFilterItem => resultFilterItem.id === filter.id))]
