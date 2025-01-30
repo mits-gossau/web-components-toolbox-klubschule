abad5236d9a66a0069a3e4c9d60b4b3203d43503
Alexander Schmidt <alexander@schmidt.eco>
2025-01-30 11:59:39 +0100

    chore(FilterCategories.js): remove unnecessary comments [MIDUWEB-1845]

--- src/es/components/molecules/filterCategories/FilterCategories.js
+++ src/es/components/molecules/filterCategories/FilterCategories.js
@@ -20,7 +20,6 @@ export default class FilterCategories extends Shadow() {
     this.generateFilterMap = new Map()
     this.total = 0
     this.firstTreeItem = null
-    this.mainLevelNav = null
 
     this.withFacetEventListener = event => this.renderHTML(event.detail.fetch)
 
@@ -483,7 +482,6 @@ export default class FilterCategories extends Shadow() {
     const mainNav = document.createElement('div')
     mainNav.setAttribute('class', 'main-level')
     
-    // this.addEventListenersToDialogs(mainNav)
     this.observeMainNav(mainNav)
     
     this.html = mainNav
