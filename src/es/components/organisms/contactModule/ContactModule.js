dba6dae3708954d5fccddc9ef0ab9421a661de3b
Weedshaker <weedshaker@gmail.com>
2025-02-25 17:15:11 +0100

    example configs

--- src/es/components/organisms/contactModule/ContactModule.js
+++ src/es/components/organisms/contactModule/ContactModule.js
@@ -26,11 +26,36 @@ export default class ContactModule extends Grid {
       :host > section > [wide] {
         grid-column: span 2;
       }
-      :host([count-section-children="1"]) > section > *, :host > section > [wide="100%"] {
+      :host([count-section-children="1"]) > section > [wide], :host > section > [wide="100%"] {
         grid-column: span 3;
       }
+      :host > section > *.contact > * {
+        margin-bottom: var(--mdx-sys-spacing-fix-l, 2rem);
+      }
+      :host > section > *.contact > ks-m-contact-row {
+        margin-bottom: var(--mdx-sys-spacing-fix-m, 1.5rem);
+      }
+      :host > section > *.contact > *:last-child {
+        margin-bottom: 0;
+      }
+      :host > section > *.contact > a.logo {
+        display: inline-block;
+        margin-top: 0;
+        margin-right: 0;
+        margin-left: 0;
+      }
+      :host > section > *.contact > a.logo > img, :host > section > *.contact > img.logo {
+        max-height: 3.125em;
+      }
+      :host > section > *.contact > div.buttons {
+        --button-primary-width: 100%;
+        --button-secondary-width: 100%;
+        display: grid;
+        gap: var(--mdx-sys-spacing-fix-m, 1.5rem);
+        width: fit-content;
+      }
       @media only screen and (max-width: _max-width_) {
-        :host([count-section-children="1"]) > section > *, :host > section > [wide], :host > section > [wide="100%"] {
+        :host([count-section-children="1"]) > section > [wide], :host > section > [wide], :host > section > [wide="100%"] {
           grid-column: span 1;
         }
       }
