40dfab8e002241b8762cc5cc04be79f7ec528b3e
Alexander <alexander@schmidt.eco>
2025-05-14 08:10:10 +0200

    fix(WithFacet.js): improve reset handling [MIDUWEB-1931]

--- src/es/components/controllers/withFacet/WithFacet.js
+++ src/es/components/controllers/withFacet/WithFacet.js
@@ -162,7 +162,8 @@ export default class WithFacet extends WebWorker() {
         this.deleteAllFiltersFromUrl(currentRequestObj.filter)
         currentRequestObj = structuredClone(initialRequestObj)
         delete currentRequestObj.searchText
-        currentRequestObj.filter = initialFilter
+        // currentRequestObj.filter = initialFilter
+        currentRequestObj.filter = initialRequestObj.filter
         currentRequestObj.sorting = 3
         if ((this.saveLocationDataInLocalStorage || this.saveLocationDataInSessionStorage) && this.params.has('cname')) currentRequestObj.sorting = 2
       } else if (event?.type === 'reset-filter') {
--- src/es/components/molecules/filterCategories/FilterCategories.js
+++ src/es/components/molecules/filterCategories/FilterCategories.js
@@ -340,7 +340,7 @@ export default class FilterCategories extends Shadow() {
         </div>
         <div class="dialog-content">
           ${this.hasAttribute('translation-key-reset') ? /* html */`<p class="reset-link">
-            <a-button namespace="button-transparent-" request-event-name="reset-filter" filter-key="${filterItem.urlpara}">
+            <a-button namespace="button-transparent-" request-event-name="reset-filter" filter-key="${filterItem.urlpara}" filter-type="${this.firstTreeItem ? this.firstTreeItem.typ : filterItem.typ}" id="reset-filter">
               ${this.getAttribute('translation-key-reset')}<a-icon-mdx class="icon-right" icon-name="RotateLeft" size="1em"></a-icon-mdx>
             </a-button>
           </p>` : ''}
@@ -457,6 +457,8 @@ export default class FilterCategories extends Shadow() {
       fetch.then(response => {
         setTimeout(() => {
           if (response.filters.length === 0) return
+
+          this.mainNav.innerHTML = ''
 
           response.filters.forEach((filterItem) => {
             this.generateFilters(response, filterItem)
