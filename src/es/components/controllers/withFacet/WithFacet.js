b47173e41b59799b024c8d564e4f8ccca58e3e39
Alexander <alexander@schmidt.eco>
2025-06-17 16:04:19 +0200

    fix(WithFacet.js): cleanup filter on navLevelItem level 0 [MIDUWEB-1809]

--- src/es/components/controllers/withFacet/WithFacet.js
+++ src/es/components/controllers/withFacet/WithFacet.js
@@ -228,9 +228,10 @@ export default class WithFacet extends WebWorker() {
         currentCompleteFilterObj = result[0]
         const merged = [...currentRequestObj.filter, ...result[1], ...initialFilter.filter(filter => !result[1].find(resultFilterItem => resultFilterItem.id === filter.id))]
         currentRequestObj.filter = merged.filter((filter, index, self) => index === self.findIndex(f => f.id === filter.id))
-        // check currentRequestObj.filter if children exist and if so, remove the children from the filter for a cleaner request
+        // cleanup
         currentRequestObj.filter = currentRequestObj.filter.map(filter => {
-          if (filter.children && filter.children.length) filter.children = filter.children.filter(child => child.selected || child.isquick > 0)
+          if (filter.children && filter.children.length && !filter.children.some(child => child.selected || child.isquick > 0)) filter.children = []
+          if (filter.children && filter.children.length && filter.children.some(child => child.selected)) filter.skipCountUpdate = true
           return filter
         })
         this.filterOnly = true
