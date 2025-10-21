9fce718bab05994e813a0345330363ffc3b3dfda
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2025-10-21 14:16:50 +0200

    feat(WithFacet.js): add sorting of courses by start time [MIDUWEB-2084]

--- src/es/components/controllers/withFacet/WithFacet.js
+++ src/es/components/controllers/withFacet/WithFacet.js
@@ -489,12 +489,16 @@ export default class WithFacet extends WebWorker() {
             if (event?.detail?.ppage && !json.filters.length) json.filters = sessionStorage.getItem('currentFilter') ? JSON.parse(sessionStorage.getItem('currentFilter') || '[]') : currentRequestObj.filters || initialFilter || []
             json.hasSelectedFilter = hasSelectedFilter
 
-            // sort courses by start date (ascending) 
+            // sort courses by start date (ascending) and by start time
             if (this.hasAttribute('no-search-tab') && json.courses && Array.isArray(json.courses) && !this.params.has('sorting')) {
               json.courses.sort((a, b) => {
                 const dateA = new Date(a.gueltig_ab)
                 const dateB = new Date(b.gueltig_ab)
-                return dateA - dateB
+                const dateDiff = dateA - dateB
+                if (dateDiff !== 0) return dateDiff
+                const timeA = a.start_zeit || '00:00'
+                const timeB = b.start_zeit || '00:00'
+                return timeA.localeCompare(timeB)
               })
             }
 
