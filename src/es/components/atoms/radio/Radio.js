87e2d28460ee1a1f5c419bdb52bbd8d7d5be0851
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2025-12-09 09:59:57 +0100

    feat: add image of payment methods for mobile [MIDUWEB-2194]

--- src/es/components/atoms/radio/Radio.js
+++ src/es/components/atoms/radio/Radio.js
@@ -52,6 +52,10 @@ export default class Radio extends Shadow() {
         :host .wrap-image {
           padding-left: calc(var(--mdx-comp-radiobutton-padding-horizontal-default) + var(--mdx-comp-radiobutton-sizing-ellipse));
         }
+        
+        :host .wrap-image > .mobile {
+          display: none;
+        }
 
         :host .wrap.disabled {
           pointer-events: none;
@@ -151,6 +155,14 @@ export default class Radio extends Shadow() {
         @media only screen and (max-width: _max-width_) {
           :host .wrap img {
             margin-left: 0;
+          }
+
+          :host .wrap-image > .desktop {
+            display: none;
+          }
+
+          :host .wrap-image > .mobile {
+            display: block;
           }
         }
     `
